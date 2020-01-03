9acf8f6ea3ab6e4c37c86c1114331f9c5a5a9478
Nikita Sokolov <faucct@gmail.com>
2020-01-03 09:37:09 +0300

    Data

---
+++ data/array_size/plot_images.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+from glob import glob
+from itertools import groupby
+import os
+import sys
+
+all_results = []
+for path in glob(os.path.join(sys.path[0], '*/results.tsv')):
+    with open(path) as file:
+        header = next(file)[:-1].split("\t")
+        for line in file:
+            all_results.append(line[:-1].split("\t"))
+# results = list(zip(*results))
+for y in ['Server request duration', 'Processing request duration', 'Client request duration']:
+    legend = []
+    for architecture, results in groupby(all_results, lambda result: result[0]):
+        results = list(results)
+        legend.append(architecture)
+        plt.plot(
+            [int(dict(zip(header, result))['Array size']) for result in results],
+            [float(dict(zip(header, result))[y]) / 1E6 for result in results],
+        )
+    plt.legend(legend)
+    plt.xlabel('Array size')
+    plt.ylabel(y + ' (ms)')
+    plt.title('Clients number=30. Delay=0')
+    plt.draw()
+    plt.savefig(os.path.join(sys.path[0], y + '.png'))
+    plt.show()
