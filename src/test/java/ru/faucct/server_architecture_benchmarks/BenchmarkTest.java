70b05c950e0eab5ad4d9c2b68a782a52f8ed9d41
Nikita Sokolov <faucct@gmail.com>
2020-01-04 11:34:06 +0300

    Fast benchmark test

--- src/test/java/ru/faucct/server_architecture_benchmarks/BenchmarkTest.java
+++ src/test/java/ru/faucct/server_architecture_benchmarks/BenchmarkTest.java
@@ -9,7 +9,7 @@ class BenchmarkTest {
     @Test
     void test() throws IOException, InterruptedException {
         new Benchmark(
-                new Benchmark.Config(Benchmark.Config.Architecture.Asynchronous, 10000, 50, 1_000_000_000, 10)
+                new Benchmark.Config(Benchmark.Config.Architecture.Asynchronous, 10, 50, 0, 10)
         ).local();
     }
 
